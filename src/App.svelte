1a36765cf34adb60d8d52b56544387dedaf2c0fa
Víctor <victorgd97@gmail.com>
2021-06-05 17:45:47 +0200

    1

--- src/App.svelte
+++ src/App.svelte
@@ -1,101 +1,14 @@
 <script>
-	//Some components
-	import Line from './components/charts/Line.svelte'
-	import Area from './components/charts/Area.svelte'
-	import Bars from './components/charts/Bars.svelte'
-	import Multiline from './components/charts/Multiline.svelte'
-	import Scatter from './components/charts/Scatter.svelte'
-	import ScatterCanvas from './components/charts/ScatterCanvas.svelte'
-	import Mapbox from './components/maps/Mapbox.svelte'
-	import Map from './components/maps/Map.svelte'
-	import Scroller from '@sveltejs/svelte-scroller'
-  	import Sankey from './components/charts/Sankey.svelte'
+	import Budget from './components/Budget.svelte'
 
-	//Test data
-	import world from './data/world.json';
-	import cases from './data/covid-cases.json';
-	import weather from './data/weather.json';
-	import weather2 from './data/weather2.json';
-	import weather3 from './data/weather3.json';
-	import sankeydata from './data/sankey-data.js';
-
-	import locale from '@reuters-graphics/d3-locale';
-	import { geoWinkel3 } from 'd3-geo-projection';
-	import { scaleQuantize } from 'd3-scale';
-	import {extent} from 'd3-array';
-	
-	let color = '#5cc6b2';
-
-	let index = 0, offset, progress;
-
-	let scatterStep, otherStep;
-
-	const loc = new locale('es');
-	const format = {
-		x: loc.formatTime('%b %e'),
-        y: loc.format(',.1d'),
-    }
-
-	weather.forEach(d => d.time = new Date(d.time));
-	weather2.forEach(d => d.time = new Date(d.time));
-
-	const projection = geoWinkel3()
-				.rotate([-11, 0])
-				.precision(1);
-
-	console.log(cases)
-
-	const palette = () => {
-		const _extent = extent(cases.data, d => d.latest.cases)
-		const max = _extent[0] > _extent[1] ? _extent[0] : _extent[1];
-		const min = _extent[0] < _extent[1] ? _extent[0] : _extent[1];
-		const d = (max-min)/9;
-
-		return scaleQuantize()
-				.range(['#ffe461', '#ffc755', '#fea94d', '#f68c4a', '#ea704a', '#da554e', '#c73a55', '#ae1f5f', '#90006c'])
-				.domain([... Array(9)].map((_d, i) => min + d * i))
-				.nice();
-	}
-
-	const steps = [
-		{center: [-7.889722,42.782222], zoom:7.5},
-		{center: [0,42.782222], zoom:7},
-		{center: [-7.889722,41.782222], zoom:6.5}
-	]
-
-	const points = [...new Array(2500)]
-        .map(d => (
-            {
-				coords: [
-					{ x: Math.random() * 400,
-					y: Math.random() * 400,
-					width: Math.random() * 16,
-					height: Math.random() * 16 },
-					{ x: Math.random() * 400,
-					y: Math.random() * 400,
-					width: Math.random() * 16,
-					height: Math.random() * 16 }
-				]
-        }))
-
-	const otherPoints = [...new Array(800)]
-        .map(d => (
-            {
-				coords: [
-					{ x: Math.random() * 400,
-					y: Math.random() * 400,
-					r: Math.random() * 16 },
-					{ x: Math.random() * 400,
-					y: Math.random() * 400,
-					r: Math.random() * 16 }
-				]
-        }))
 
 	
 </script>
 
 <main>
 
+	<Budget
+	/>
 	<Multiline 
 		data={weather2}
 		title='Title' desc='Description'
---
+++ src/components/Budget.svelte
@@ -0,0 +1,37 @@
+<script>
+
+function toggle () {
+     data_modified.map(d => {
+        d.active = d.active ? false : true;
+        return d;
+        })
+    }
+</script>
+
+<main>
+ <div class="budgetBar">
+  
+ </div>
+
+<div>
+    {#each data_modified as data}
+        <p>
+            {d.description}
+        </p>
+
+        <div>
+            <button on:click={toggle}
+            >{d.action}
+          </button>
+        </div>
+    {/each}
+</div>
+
+</main>
+
+<style>
+	.budgetBar {
+		position: sticky;
+		margin: 0 auto;
+	}
+</style>
