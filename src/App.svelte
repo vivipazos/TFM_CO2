d7139ebee9a93c7d8aad4370eb850fde9bf8d36f
Víctor <victorgd97@gmail.com>
2021-06-10 13:33:36 +0200

    transition

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,11 @@
 	import Budget from './components/Budget.svelte'
 	import Action from './components/Action.svelte'
 	import Text from './components/common/Text.svelte'
+	import Yearly from './data/YearlyData.json'
+	import Scroller from '@sveltejs/svelte-scroller';
+	import { each } from 'svelte/internal';
+  
+  	let index = 0, offset, progress;
 
 	export let content;
 	export let actions;
@@ -11,11 +16,27 @@
           d.active = false;
           return d;
         })
-	
-	$:console.log(data_modified);
 </script>
 
 <main>
+	<Scroller top={0} bottom={0.8} bind:index bind:offset bind:progress>
+		<div slot="background">
+			<Budget
+				year = {Yearly[index].year}
+				carbon = {Yearly[index].carbonDioxide}
+				percentage = {Yearly[index].Percentage}
+				widthV = {parseFloat(Yearly[index].Percentage).toFixed(2).toString() + "%"}
+			/>
+
+		  <p>Section {index + 1} is currently active.</p>
+		</div>
+	  
+		<div slot="foreground">
+			{#each Yearly as year}
+				<section>Text</section>
+			{/each}
+		</div>
+	</Scroller>
 	{#each content as block}
 
 	{#if block.type === 'text'}
@@ -45,6 +66,8 @@
 		margin: 0 auto;
 	}
 
-	
+	section {
+		height: 80vh; 
+	}
 	
 </style>
--- src/components/Budget.svelte
+++ src/components/Budget.svelte
@@ -2,12 +2,20 @@
 export let action
 export let carbon
 
+export let year
+export let percentage
+export let widthV
+
+$: console.log(widthV)
 let baseValue = 46051;
 let carbonLimit = 2620000;
 
+//$: if(percentage)
+//{widthV = percentage.toFixed(2).toString() + "%"}
+/* 
 $:selectedAction = action.filter(d => d.active)
     .map(d => +d.amount_all)
-    .reduce((a,c) => a + c, 0)
+    .reduce((a,c) => a + c, 0) 
 
 $:modifiedValue = baseValue - selectedAction
 
@@ -16,26 +24,41 @@ $:console.log(modifiedValue)
 let lastValue = carbon[carbon.length - 1]
 
 console.log(lastValue)
-
+*/
 // function {
 // }
 
 </script>
+{#if year}
+<div style="--widthV: {widthV}" class="budgetBarYearly">
+
+</div>
+{/if}
 
  <div class="budgetBar">
 
  </div>
 
 
-
 <style>
 	.budgetBar {
 		position: sticky;
         top: 0;
+        left: 0;
 		margin: 0 auto;
         background-color: rgb(204, 0, 51);
         width: 800px;
         height: 200px;
+	}
+
+    .budgetBarYearly {
+        top: 0;
+        left: 0;
+		margin: 0;
+        background-color: rgb(204, 0, 51);
+        transition: width 2s;
+        width: var(--widthV);
+        height: 100vh;
 	}
 
 </style>
