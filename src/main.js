71c2cbbf414241e3b5096984ab35648c479e4e49
Rebecca Pazos <vivi.pazos@me.com>
2021-06-13 17:50:08 +0800

    running gdocs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import App from './App.svelte';
 import story from './data/story.json'
 import actions from './data/actions.json'
-import carbon  from './data/carbon.json'
+import carbon from './data/carbon.json'
 
 const app = new App({
     target: document.body,
