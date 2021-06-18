b4e775f00135ee27cab39aa88518e01aceff2c7b
xaquingv <xocasgv@gmail.com>
2021-06-18 18:25:41 +0200

    Update BudgetActions.svelte

--- src/components/BudgetActions.svelte
+++ src/components/BudgetActions.svelte
@@ -3,11 +3,9 @@ import {scaleLinear} from 'd3-scale'
 export let action;
 export let carbon;
 
-console.log(carbon)
-
 let baseValue = 17000; //Mt CO2, constant yearly increase if nothing done
 let carbonEnd  = 2721042; //Mt CO2, the amount at the edge of the allowed budget before reaching 1.5 degrees
-let carbonStart = carbon[carbon.length - 1].carbonDioxide;
+let carbonStart = +carbon[carbon.length - 1].carbonDioxide;
 let selectedActions = action;
 let actives_sum = 0;
 let growth = +carbonStart;
@@ -17,7 +15,7 @@ let yearlyTime = 5000; /* In milliseconds */
 
 let scale = scaleLinear()
     .domain([carbonStart, carbonEnd])
-    .range([0,100])
+    .range([0,60])
 
 $: if (action) {
     selectedActions = action.filter(d => d.active === true);
@@ -25,18 +23,26 @@ $: if (action) {
 }
 
 $:modifiedValue = baseValue - actives_sum;
+$:console.log(modifiedValue)
 
 const grow = () => {
     let pace = modifiedValue / (yearlyTime / interval);
     growth += pace;
-    console.log('Growth: ' + growth, 'Pace: ' + pace, 'ModifiedValue' + modifiedValue);
 }
 
 setInterval(grow, interval);
 
+$:yearEnd = 2020 + Math.floor((carbonEnd - carbonStart) / modifiedValue);
+
 </script>
     
-<div class="budgetBar" style="width:{scale(growth)}vw"></div> 
+<div class="budgetBar" style="width:{scale(growth)}vw">
+    <p>{yearEnd}</p>
+    <video title= "The widht of the red is the accumulated emissions. There is some uncertainty in the numbers, that is why the edge is not precisely defined" autoplay muted loop>
+        <source src="./smoke_edge_loop2.mp4" type="video/mp4">
+    </video>
+</div>
+
     
 <style>
     .budgetBar {
@@ -128,11 +134,14 @@ setInterval(grow, interval);
 
     video {
         position: absolute;
-        height: 100vh;
-        width: 30vw;
-        left: calc(var(--widthV) - 15vw);
+        height: 200px;
+        width: 40vw;
         object-fit: fill;
-        offset: 0 0 0 3em;
         pointer-events: all;
+        right: -40vw;
+        top:0;
+        margin:0;
+        padding: 0;
+
     }
 </style>
